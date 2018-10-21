88018a4218c09dd39b9614832aae0e4248944cb5
Victor Chen <vc8724@utexas.edu>
2018-10-21 11:49:42 -0500

    applied styles to view

--- view.py
+++ view.py
@@ -12,15 +12,50 @@ html = """<! DOCTYPE html>
 	<head>
 		<title>Dog View</title>
 		<meta charset = "Utf-8"/>
+		
+		<style>
+html{
+	color: #0F1C2D;
+	background-color: #7AA1D3;
+	font-family: Verdana, Geneva, sans-serif;
+}
+#doggo{
+	background-color: #E29CA4;
+	padding: 20px;
+	border-radius: 20px;
+	text-align: center;
+
+}
+#c1{
+	text-align:center;
+}
+#p1{
+	position: relative;
+	top: 30%;
+	left: 40%;
+}
+#c2{
+	position: relative;
+	top: 40%;
+	left: 30%;
+}
+#d{
+	position: relative;
+	top: 70%;
+	left: 20%;
+}
+	</style>
 	</head>
 	
 	<body>
+	<h1 id = "doggo">Dog-go</h1>
 	<h1>{}</h1>
 	<p>Here is the nearest dog of that breed looking for a home</p>
-	<img src = "{}" alt = "good doggo" width = "150%"/>
+	<img src = "../golden.jpeg" alt = "good doggo" width = "30%" id = "doggo"/>
 	<h2>Meet {}! Age: {}</h2>
 	{}
 	<p>Description:</p>
 	{}
 	</body>
+
 </html>"""
