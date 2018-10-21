f0d7ddb82bafb0445636a547b49f4d28f32b7d00
Victor Chen <vc8724@utexas.edu>
2018-10-21 10:58:49 -0500

    css + update html

--- view.py
+++ view.py
@@ -1,4 +1,13 @@
-html = f"<! DOCTYPE html>	
+#breed_message = what message to display
+#photo = (put inside of an <img src = {photo}/>)
+#name = name of nearest dog
+#age = age of dog
+#sex = sex of dog
+#descript = descript of dog
+
+#Here is the nearest dog of that breed looking for a home[at the first <p>]
+
+html = """<! DOCTYPE html>	
 <html lang = "en">
 	<head>
 		<title>Dog View</title>
@@ -6,6 +15,12 @@ html = f"<! DOCTYPE html>
 	</head>
 	
 	<body>
-	<h1>hello {hi}</h1>
+	<h1>{}</h1>
+	<p>Here is the nearest dog of that breed looking for a home</p>
+	<img src = "{}" alt = "good doggo"/>
+	<h2>{} age:{}</h2>
+	{}
+	<p>Description:</p>
+	{}
 	</body>
-</html>"
+</html>"""
